ffab53a0d8f36a82573092f174ff482849af84d1
Daniel Malekzadeh <daniel.malekzadeh.h@gmail.com>
2020-06-06 02:39:09 +0430

    write the upload template.

--- index.php
+++ index.php
@@ -5,9 +5,31 @@
 		<title> Combo Maker v1.0 </title>
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+		<style type="text/css">
+			.paddig{
+				paddig: 7px;
+			}
+		</div>
 	</head>
 	<body>
-		
+		<div class="container">
+			<div class="row">
+				<form action="" method="post" enctype="multipart/form-data">
+					<div class="col-sm-6 paddig">
+						<div class="custom-file">
+							<input type="file" class="custom-file-input" id="customFile">
+							<label class="custom-file-label" for="customFile">Choose file</label>
+						</div>
+					</div>
+					<div class="col-sm-6 paddig">
+						<div class="custom-file">
+							<input type="file" class="custom-file-input" id="customFile">
+							<label class="custom-file-label" for="customFile">Choose file</label>
+						</div>
+					</div>
+				</form>
+			</div>
+		</div>
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
