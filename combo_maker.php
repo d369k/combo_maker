fbeac6245dca1d3015cb62e28cdc7289236de37c
Daniel Malekzadeh <daniel.malekzadeh.h@gmail.com>
2020-06-07 00:37:41 +0430

    fixed bug

--- combo_maker.php
+++ combo_maker.php
@@ -1,4 +1,5 @@
 <?php
+	set_time_limit(5000);
 	if(is_file("username.txt") and is_file("password.txt") and is_file("delimiter.txt")){
 		$username_list = file("username.txt");
 		$password_list = file("password.txt");
@@ -8,14 +9,14 @@
 		}else{
 			touch("combo.txt");
 		}
-		$open_file = fopen("combo.txt","a+");
 		for($i = 0; $i < count($username_list); $i++){
 			for($j = 0; $j < count($password_list); $j++){
-				$combo_txt = trim($username_list[$i])."$delimiter".trim($password_list[$j]).PHP_EOL;
-				fwrite($open_file,$combo_txt);
+				$combo_txt = trim($username_list[$i])."$delimiter".trim($password_list[$j])."\n";
+				$combo = file_get_contents("combo.txt");
+				$combo .= $combo_txt;
+				file_put_contents("combo.txt",$combo);
 			}
 		}
-		fclose($open_file);
 		echo "combo list created , file name : combo.txt";
 	}else{
 		echo "username or password or delimiter file not found";
